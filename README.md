3dd5d37c3a5647744197246b20a487be752d3b07
Jason Hicks <jaszhix@gmail.com>
2016-11-25 05:32:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Icing Menu
+=============
+
+This is a fork of the default Cinnamon 3.2 menu applet, with more customization options, and some optimizations.
+
+### Features
+
+ * Set the height and width of the menu.
+ * Move the search box and app info area to the top, bottom, or hide them.
+ * Hide the scrollbar.
+
+Tested on Cinnamon 3.0.7 and Cinnamon 3.2.2.
+
+### Contributing
+
+*  Use [Node 6.x](https://github.com/nodesource/distributions).
+```sh
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+```
+*  Install node modules: ```npm install```
+*  Install gulp globally if you haven't already. ```sudo npm install -g gulp```
+*  Start transpile watch task: ```gulp spawn-watch```
+*  Monitor logging output: ```tail -f -n100 ~/.cinnamon/glass.log```
---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+var del = require('del');
+var babel = require('gulp-babel');
+var clear = require('clear');
+var exec = require('child_process').exec;
+
+gulp.task('package', ()=> {
+  return gulp.src('./IcingMenu@json/**/**/*')
+    .pipe(zip('ITM-dist-' + Date.now() + '.zip'))
+    .pipe(gulp.dest('./builds'));
+});
+
+gulp.task('copy', ()=> {
+  del.sync(['./IcingMenu@json/**/**/*']);
+  return gulp.src('./src/**/**/*')
+    .pipe(gulp.dest('./IcingMenu@json/'));
+});
+
+gulp.task('transpile', ['copy'], () =>
+  gulp.src('./src/*.js')
+    .pipe(babel({
+      presets: [
+        'es2015',
+        [
+          'env', 
+          {
+            targets: {
+              firefox: 31 
+            }
+          }
+        ]
+      ],
+      plugins: [
+        [
+          'transform-es2015-classes',
+          {
+            loose: true
+          }
+        ],
+        [
+          'babel-plugin-transform-builtin-extend',
+          {
+            globals: ['Error', 'Array', 'Set', 'Object'],
+            approximate: false
+          }
+        ],
+        'transform-es2015-parameters',
+        'transform-es2015-destructuring'
+      ],
+      compact: false
+    }))
+    .pipe(gulp.dest('IcingMenu@json'))
+);
+
+gulp.task('install', ['transpile'], (cb)=>{
+  exec('cp -avrf  IcingMenu@json ~/.local/share/cinnamon/applets/', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+gulp.task('reload', ['install'], (cb)=>{
+  exec(`dbus-send --session --dest=org.Cinnamon.LookingGlass --type=method_call /org/Cinnamon/LookingGlass org.Cinnamon.LookingGlass.ReloadExtension string:'IcingMenu@json' string:'APPLET'`, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+gulp.task('watch', ()=> {
+  gulp.watch('./src/*.{js,json,py,css,md,po}', ['reload']);
+});
+
+gulp.task('clear-terminal', ()=> {
+  clear();
+});
+
+gulp.task('spawn-watch', ['clear-terminal'], ()=> {
+ var spawnWatch = ()=> {
+    var proc = require('child_process').spawn('gulp', ['watch'], {stdio: 'inherit'});
+    proc.on('close', function (code) {
+      spawnWatch();
+    });
+  };
+  spawnWatch();
+});
+gulp.task('default', ['spawn-watch'], ()=> {});
